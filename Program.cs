3bc70aaed93dc6c290c407a3cf9dd31ce60262d3
vlada <vsh_o@ukr.net>
2022-11-05 23:05:24 +0200

    First commit

---
+++ Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+
+namespace Program
+{
+    class MyDictionary<Tkey, Tval>
+    {
+        private int counter = 0;
+        private Tkey[] keysArray = null;
+        private Tval[] valsArray = null;
+
+        public int Counter
+        {
+            get { return this.counter; }
+        }
+
+        public void Add(Tkey key, Tval val)
+        {
+            this.counter++;
+
+            Array.Resize(ref keysArray, counter);
+            keysArray[counter - 1] = key;
+
+            Array.Resize(ref valsArray, counter);
+            valsArray[counter - 1] = val;
+        }
+
+        public Tval this[Tkey key]
+        {
+            get
+            {
+                int ind = 0;
+                for (int i = 0; i < keysArray.Length; i++)
+                {
+                    if (key.Equals(keysArray[i]))
+                        ind = i;
+                }
+                return valsArray[ind];
+            }
+        }
+    }
+}
